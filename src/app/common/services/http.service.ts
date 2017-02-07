84c9b2296ade2f50bcfbb49395af3cf1d2b5c221
Guenther <gzoppelt1@gmail.com>
2017-02-07 11:02:01 +0000

    http done

--- src/app/common/services/http.service.ts
+++ src/app/common/services/http.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {Http, Response, Headers} from "@angular/http";
 import 'rxjs/Rx';
+import {Observable} from "rxjs/Rx";
 
 
 @Injectable()
@@ -10,11 +11,35 @@ export class HttpService {
     private http: Http
   ) { }
 
-  getData() {
+  getDataStringForAsync() {
     return this.http
       .get(this.baseUrl + 'title.json')
-      .map( (response: Response) => response.json() )
+      .map( (response: Response) => this.checkNull(response).json() )
     ;
+  }
+
+  getDataStringForAsyncError() {
+    return this.http
+      .get(this.baseUrl + 'doesNotExist.json')
+      .map( (response: Response) => this.checkNull(response).json() )
+      .catch(this.handleError)
+    ;
+  }
+
+  getDataObjectForSubscribe() {
+    return this.http
+      .get(this.baseUrl + 'title.json')
+      .map( (response: Response) => this.checkNull(response) )
+      .catch(this.handleError)
+      ;
+  }
+
+  getDataObjectForSubscribeError() {
+    return this.http
+      .get(this.baseUrl + 'doesNotExist.json')
+      .map( (response: Response) => this.checkNull(response) )
+      .catch(this.handleError)
+      ;
   }
 
   sendData(user: any) {
@@ -24,8 +49,28 @@ export class HttpService {
     return this.http
         .post(this.baseUrl + 'user.json', body, {headers: headers})
         .map( (data:Response) => data.json() )
+        .catch(this.handleError)
     ;
+  }
 
+  sendToReadOnly(title: string, name: string, sex: string) {
+    let user = {
+      title,
+      name,
+      sex
+    };
+    const body = JSON.stringify(user);
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    return this.http
+      .post(this.baseUrl + 'readOnly.json', body, {headers: headers})
+      .map( (data:Response) => {
+        console.log(data);
+        console.log(data.json);
+        return data.json();
+      })
+      .catch(this.handleError)
+    ;
   }
 
   getUser() {
@@ -33,5 +78,33 @@ export class HttpService {
         .get(this.baseUrl + 'user.json')
         .map( (response: Response) => response.json() )
         ;
+  }
+
+  // If resource does not exist Firebase returns NULL.
+  private checkNull(res: Response): Response  {
+        if (!res.json()) {
+          console.log('checkNull():');
+          console.log(res);
+          //return Observable.throw('Requested ressource does not exist.')
+          throw new Error('Requested ressource does not exist.');
+        } else {
+          return res;
+    }
+  }
+
+  private handleError(err) {
+    console.log('HttpService.handleError(err):')
+    console.log(err);
+    /*
+    if (res.status < 200 || res.status >= 300) {
+      // Do something nice with this error, like loggong, mailing, etc.
+      // You can not really catch it as it has to be thrown again due to the signiture
+      console.log('HttpService.handleError():')
+      console.log('This request has failed: ' + res.status + ' - ' + res.statusText + ' - ' + res.json().error);
+
+
+    }
+    */
+    return Observable.throw(err);
   }
 }
--- src/app/http/http.component.ts
+++ src/app/http/http.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpService} from "app/common/services/http.service";
 import {Response} from "@angular/http";
+import {Observable} from "rxjs";
 
 class User {
   username: string = '';
@@ -18,29 +19,84 @@ class User {
 })
 export class HttpComponent implements OnInit {
 
-  titleValue: string = '';
+  // to be set in onNgInit()
+  titleString: string = '';
+  titleError: string = '';
+
+  asyncObservable: Observable<any>;
+  asyncError: Observable<any>;
+  realError: string = '';
+
+
   postResponse: string = '';
   answerArrived: boolean = false;
   users: User[] = [];
+  sendError: string = '';
+  postError: string = '';
 
   constructor(
     private httpService: HttpService
-  ) { }
+  ) {
+
+}
 
   ngOnInit() {
-    this.httpService.getData().subscribe(
-        (data: any) => this.titleValue = data
 
+    this.httpService.getDataObjectForSubscribe().subscribe(
+      (data: any) => this.titleString = data.json()
     );
+
+    // to demonstrate error handling by httpService
+    this.httpService.getDataObjectForSubscribeError().subscribe(
+      (data) => this.titleError = data.json(),
+      (err)  => this.titleError = err.message
+    );
+
+    this.asyncObservable = this.httpService.getDataStringForAsync();
+
+    this.asyncError  = this.httpService
+    //asyncError is bound to async pipe, which accepts Observables, but will not display strings
+      .getDataStringForAsyncError()
+      .catch ( (err) => {
+        this.realError = err.message;
+        // realError has no pipe and accepts the error message as string
+        return Observable.throw('none');
+        // catch forces to return an ObservableInput<any, {}>
+        // this one will live on uncaught as "Exception: none"
+      })
+    ;
+
   }
 
   onSubmit(username, email) {
+    this.sendError = '';
     this.httpService
         .sendData({username: username, email: email})
-        .subscribe( (data) => {
-          this.postResponse = data.name;
-          this.answerArrived = true;
-        })
+        .subscribe(
+          (data) => {
+            this.postResponse = data.name;
+            this.answerArrived = true;
+          },
+          (res: Response) => {
+              console.log('error handled by subscription: ');
+              console.log('status: ' + res.status);
+              console.log('statusText: ' + res.statusText);
+              console.log('error: ' + res.json().error);
+              this.sendError = res.json().error +
+                ' - Please set ".write" in Firebase rules back to true !';
+          }
+        )
+    ;
+  }
+
+  onSubmitReadOnly(title, name, sex) {
+    this.postError = '';
+    this.httpService
+      .sendToReadOnly(title, name, sex)
+      .subscribe(
+        (data) => console.log(data),
+        (err)  => this.postError = err.json().error
+      )
     ;
   }
 
